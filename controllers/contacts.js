1d48dace92fee4e8e72f801e75144e5de9f70a59
CodeSevenic <sevenicbookings@gmail.com>
2023-03-17 12:06:20 +0200

    first commit

---
+++ controllers/contacts.js
@@ -0,0 +1,43 @@
+﻿require('dotenv').config();
+const axios = require('axios');
+// Set the API key
+const apiKey = process.env.API_KEY; // Replace with your HubSpot API key
+
+exports.listsObject = async (req, res) => {
+  const listData = await req.body.listIDs;
+
+  const givenIDs = [];
+
+  listData.listIDs.forEach((item) => givenIDs.push(item.list_id));
+
+  console.log('GIVEN IDS: ', givenIDs);
+
+  // Set the request headers
+  const headers = {
+    Authorization: `Bearer ${apiKey}`,
+  };
+
+  const resLists = await Promise.all(
+    givenIDs.map(async (listId, index) => {
+      const url = `https://api.hubapi.com/contacts/v1/lists/${listId}`;
+      const response = await axios.get(url, { headers });
+      const list = response.data;
+      const name = list.name;
+      const size = list.metaData.size;
+      const listID = list.listId;
+      return {
+        name,
+        listID,
+        size,
+        index,
+      };
+    })
+  );
+
+  const data = resLists.sort((a, b) => a.index - b.index);
+
+  res.status(200).json({
+    lists: data,
+    listsAll: 'Hello',
+  });
+};
---
+++ index.js
@@ -0,0 +1,31 @@
+﻿const express = require('express');
+const app = express();
+const opn = require('open');
+const cors = require('cors');
+const { listsObject } = require('./controllers/contacts');
+const port = process.env.PORT || 2000;
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+
+// Set CORS headers
+app.use(cors());
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+console.log('Hello world');
+
+app.post('/api/contact-lists', listsObject);
+
+app.get('/error', (req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.write(`<h4>Error: ${req.query.msg}</h4>`);
+  res.end();
+});
+
+app.listen(port, () => console.log(`=== Starting your app on http://localhost:${port} ===`));
+
+opn(`http://localhost:${port}`);
